552fe90a4a5e29952418a676c3adef1c1b5c660a
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2024-03-15 22:00:45 +0800

    complete usecase img2pdf

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import abc
 import os.path
 from pdf2image import convert_from_path
+import img2pdf
 
 
 class Converter(metaclass=abc.ABCMeta):
@@ -27,13 +28,28 @@ class PDFConverter(Converter):
 
 
 class IMGConverter(Converter):
-    def convert(self, images_path):
-        pass
+    def convert(self, images_path, save_path, filename_ordered, saved_filename):
+        try:
+            f_imgs = []
+            with open('{}/{}.pdf'.format(save_path, saved_filename), 'wb') as f_pdf:
+                for filename in filename_ordered:
+                    for image_path in images_path:
+                        if image_path.__contains__(filename):
+                            f_img = open(image_path, 'rb')
+                            f_imgs.append(f_img)
+                f_pdf.write(img2pdf.convert(f_imgs))
+            for f_img in f_imgs:
+                f_img.close()
+            return True
+        except:
+            return False
 
 
 if __name__ == '__main__':
     pdf_converter = PDFConverter()
-    img_converter = IMGConverter()
-
     pdf_converter.convert(r'C:\Users\TonyTTTTT\Desktop\Guitar Sheet\i really want to stay at your house TAB.pdf'
                           , r'./result', [0, 2], 'png')
+
+    img_converter = IMGConverter()
+    img_converter.convert(r'C:\Users\TonyTTTTT\Pictures\下載.jfif', r'./result')
+
--- gui.py
+++ gui.py
@@ -5,14 +5,39 @@ import pdfplumber
 from converter import *
 
 
+class DragDropListbox(tk.Listbox):
+    """ A Tkinter listbox with drag'n'drop reordering of entries. """
+    def __init__(self, master, **kw):
+        kw['selectmode'] = tk.SINGLE
+        tk.Listbox.__init__(self, master, kw)
+        self.bind('<Button-1>', self.setCurrent)
+        self.bind('<B1-Motion>', self.shiftSelection)
+        self.curIndex = None
+
+    def setCurrent(self, event):
+        self.curIndex = self.nearest(event.y)
+
+    def shiftSelection(self, event):
+        i = self.nearest(event.y)
+        if i < self.curIndex:
+            x = self.get(i)
+            self.delete(i)
+            self.insert(i+1, x)
+            self.curIndex = i
+        elif i > self.curIndex:
+            x = self.get(i)
+            self.delete(i)
+            self.insert(i-1, x)
+            self.curIndex = i
+
+
 class GUI:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Converter")
         # self.window.geometry("300x200")
 
-        self.filename = None
-        self.filename_tkVar = tk.StringVar()
+        self.use_case = tk.StringVar()
         self.page_num = None
         self.save_path = None
         self.save_path_tkVar = tk.StringVar()
@@ -23,7 +48,12 @@ class GUI:
         self.set_save_path_button = None
         self.convert_button = None
         self.save_path_label = None
-        self.page_label = None
+        self.list_label = None
+        self.filenames = ''
+        self.filenames_tkVar = tk.StringVar()
+        self.list_label_tkVar = tk.StringVar()
+        self.saved_filename_inputbox_label = None
+        self.saved_filename_inputbox = None
 
         self.frame1 = tk.Frame(self.window)
         self.frame1.pack(fill='none', expand=True, ipadx=10, ipady=20, padx=20, pady=20)
@@ -34,43 +64,74 @@ class GUI:
         self.frame3 = tk.Frame(self.window)
         self.frame3.pack(fill='none', expand=True, ipadx=10, ipady=20, padx=20, pady=20)
 
-        self.open_button = tk.Button(
+        self.use_cases = ['pdf2img', 'img2pdf']
+        self.use_case_menu = tk.OptionMenu(self.frame1, self.use_case, *self.use_cases, command=self.destroy_components)
+        self.use_case_menu.pack(expand=True)
+        self.use_case.set(self.use_cases[0])
+
+        self.select_file_button = tk.Button(
             self.frame1,
             text='Select a File',
-            command=self.add_file
+            command=self.select_file
         )
-        self.open_button.pack(expand=True)
+        self.select_file_button.pack(expand=True)
 
         self.window.mainloop()
 
-    def add_file(self):
-        filetypes = (
-            ('pdf files', '*.pdf'),
-            ('All files', '*.*')
-        )
+    def destroy_components(self, *args):
+        for widget in self.frame2.winfo_children():
+            widget.pack_forget()
+        for widget in self.frame3.winfo_children():
+            widget.pack_forget()
+        self.filenames_tkVar.set('')
+        self.list = None
 
-        self.filename = fd.askopenfilename(
-            title='Open a file',
-            initialdir='./',
-            filetypes=filetypes)
+    def select_file(self):
+        if self.use_case.get() == self.use_cases[0]:
+            filetypes = (
+                ('pdf files', '*.pdf'),
+                ('All files', '*.*')
+            )
 
-        if self.filename == '':
+            self.filenames = fd.askopenfilename(
+                title='Select one pdf file',
+                initialdir='./',
+                filetypes=filetypes)
+        elif self.use_case.get() == self.use_cases[1]:
+            filetypes = (
+                ('img files', '*.jpg *.png'),
+                ('All files', '*.*')
+            )
+
+            self.filenames = fd.askopenfilenames(
+                title='Select one or multiple img file',
+                initialdir='./',
+                filetypes=filetypes)
+
+
+        if self.filenames == '':
             showinfo(title='Error', message='No file selected!')
         else:
             self.show_filename()
             self.create_selecting_components()
 
-            if self.filename.split('.')[1] == 'pdf':
-                pdf_file = pdfplumber.open(self.filename)
+            if type(self.filenames) == tuple:
+                self.update_imgs_list()
+                self.create_saved_filename_inputbox()
+            elif self.filenames.split('.')[1] == 'pdf':
+                pdf_file = pdfplumber.open(self.filenames)
                 total_pages = len(pdf_file.pages)
-                self.update_list(total_pages)
+                self.update_pdf_list(total_pages)
                 self.create_img_type_menu()
+
+    # def create_add_file_btn(self):
+
 
     def show_filename(self):
         if self.filename_label == None:
-            self.filename_label = tk.Label(self.frame1, textvariable=self.filename_tkVar, wraplength=200)
-            self.filename_label.pack(expand=True)
-        self.filename_tkVar.set(self.filename)
+            self.filename_label = tk.Label(self.frame1, textvariable=self.filenames_tkVar, wraplength=200)
+        self.filename_label.pack(expand=True)
+        self.filenames_tkVar.set(self.filenames)
 
     def set_save_path(self):
         self.save_path = fd.askdirectory(
@@ -86,16 +147,24 @@ class GUI:
     def show_save_path(self):
         if self.save_path_label == None:
             self.save_path_label = tk.Label(self.frame3, textvariable=self.save_path_tkVar, wraplength=200)
-            self.save_path_label.pack(expand=True)
+        self.save_path_label.pack(expand=True)
         self.save_path_tkVar.set(self.save_path)
 
     def create_selecting_components(self):
-        if self.page_label == None:
-            self.page_label = tk.Label(self.frame2, text='Pages:')
-            self.page_label.pack(expand=True)
+        if self.list_label == None:
+            self.list_label = tk.Label(self.frame2, textvariable=self.list_label_tkVar)
+        if self.use_case.get() == self.use_cases[0]:
+            self.list_label_tkVar.set('Pages:')
+        elif self.use_case.get() == self.use_cases[1]:
+            self.list_label_tkVar.set('Pages arrange:')
+        self.list_label.pack(expand=True)
+
         if self.list == None:
-            self.list = tk.Listbox(self.frame2, selectmode='multiple', font=16)
-            self.list.pack(expand=True)
+            if self.use_case.get() == self.use_cases[0]:
+                self.list = tk.Listbox(self.frame2, selectmode='multiple', font=16)
+            elif self.use_case.get() == self.use_cases[1]:
+                self.list = DragDropListbox(self.frame2, selectmode='multiple', font=16, )
+        self.list.pack(expand=True)
 
         if self.set_save_path_button == None:
             self.set_save_path_button = tk.Button(
@@ -103,18 +172,31 @@ class GUI:
                 text='Select a Path to save',
                 command=self.set_save_path
             )
-            self.set_save_path_button.pack(expand=True)
+        self.set_save_path_button.pack(expand=True)
 
-    def update_list(self, total_pages):
+    def update_pdf_list(self, total_pages):
         self.list.delete(0, tk.END)
         for i in range(1, total_pages+1):
             self.list.insert(i, i)
+
+    def update_imgs_list(self):
+        self.list.delete(0, tk.END)
+        for i in range(0, len(self.filenames)):
+            filename = os.path.split(self.filenames[i])[1]
+            self.list.insert(i, filename)
+
+    def create_saved_filename_inputbox(self):
+        self.saved_filename_inputbox_label = tk.Label(self.frame2, text='saved filename:')
+        self.saved_filename_inputbox_label.pack(expand=True)
+        self.saved_filename_inputbox = tk.Entry(self.frame2)
+        self.saved_filename_inputbox.insert(0, 'IMGs')
+        self.saved_filename_inputbox.pack(expand=True)
 
     def create_img_type_menu(self):
         img_types = ['jpg', 'png']
         if self.img_type_menu == None:
             self.img_type_menu = tk.OptionMenu(self.frame2, self.img_type, *img_types)
-            self.img_type_menu.pack(expand=True)
+        self.img_type_menu.pack(expand=True)
         self.img_type.set(img_types[0])
 
     def create_convert_btn(self):
@@ -124,11 +206,16 @@ class GUI:
                 text='convert',
                 command=self.convert
             )
-            self.convert_button.pack(expand=True)
+        self.convert_button.pack(expand=True)
 
     def convert(self):
-        pdf_converter = PDFConverter()
-        success = pdf_converter.convert(self.filename, self.save_path, self.list.curselection(), self.img_type.get())
+        if self.use_case.get() == self.use_cases[0]:
+            converter = PDFConverter()
+            success = converter.convert(self.filenames, self.save_path, self.list.curselection(), self.img_type.get())
+        elif self.use_case.get() == self.use_cases[1]:
+            converter = IMGConverter()
+            success = converter.convert(self.filenames, self.save_path, self.list.get(0, tk.END), self.saved_filename_inputbox.get())
+
         if success:
             showinfo(title='Complete', message='Success convert!')
         else:
